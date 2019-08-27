c25e740e299d85981c32616b0b9026e97674da46
Gaurav <gvsharma@gmail.com>
2019-08-27 11:47:24 +0530

    Add installation steps for Gauntit

---
+++ gauntit/install.sh
@@ -0,0 +1,11 @@
+echo "Check ruby version"
+`ruby --version`
+
+echo "Installing Ruby"
+apt install ruby
+
+echo "Installing Ruby-Dev"
+apt-get install ruby-dev
+
+echo "Installing Gauntit"
+gem install gauntlt
