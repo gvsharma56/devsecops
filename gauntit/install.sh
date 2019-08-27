3257fa05716098785d5320adabfc335c1856dc2f
Gaurav <gvsharma@gmail.com>
2019-08-27 12:11:46 +0530

    Added instructions for dependencies

--- gauntit/install.sh
+++ gauntit/install.sh
@@ -7,5 +7,13 @@ apt install ruby
 echo "Installing Ruby-Dev"
 apt-get install ruby-dev
 
+
+echo "Installing dependencies"
+apt-get install  zlib1g-dev
+gem install rake
+gem install aruba
+gem install mini_portile2
+gem install nokogiri
+
 echo "Installing Gauntit"
 gem install gauntlt
